e3f227e96632c890b5c281c966095d308585e901
TDL3 <29535697+TDL3@users.noreply.github.com>
2021-02-14 20:11:19 +0800

    feature: Added a button to open the result page

--- get-all-links/README.md
+++ get-all-links/README.md
@@ -4,3 +4,12 @@ Get all links from a website, change @match to the website to which you want to 
 获取网页中的全部链接，将 @match 改到你想获得链接的网站。  
 ## Screenshot
 ![table](table.png)
+
+## Example
+### Get all links from a youutbe channel
+1. change `@match` to `https://youtube.com/*`
+2. change regex to `watch`
+```javascript
+const name_regex = new RegExp(/watch/g);
+```
+![youtube](youtube.png)
--- get-all-links/get-all-lnks.js
+++ get-all-links/get-all-lnks.js
@@ -4,7 +4,7 @@
 // @name:zh-HK   獲取網頁中的全部鏈接
 // @name:zh-CN   獲取網頁中的全部鏈接
 // @namespace    https://tdl3.com/
-// @version      0.1.0
+// @version      0.2.0
 // @description  Get all links from a website, change @match to the website to which you want to get links.
 // @description:zh-TW  获取网页中的全部链接，将 @match 改到你想获得链接的网站。
 // @description:zh-HK  獲取網頁中的全部鏈接，將 @match 改到你想獲得鏈接的網站 。
@@ -15,6 +15,9 @@
 // @run-at       document-idle
 // ==/UserScript==
 
+const filter_results = true;
+const filter_regex = new RegExp(/png|jpg/g);
+
 function make_table(links_and_names) {
   let table = "<table><thead><th>Names</th><th>Links</th></thead><tbody>";
   for (const [link, name] of Object.entries(links_and_names)) {
@@ -24,32 +27,72 @@ function make_table(links_and_names) {
   window.open("").document.write(table);
 }
 
-function make_list(links) {
+function make_list(links_and_names) {
   let list = "";
-  for (let link in links) {
-    list += `<div>${links[link]}</div>`;
-  }
+  Object.keys(links_and_names).forEach(link => {
+    list += `<div>${link}</div>`;
+  });
+
   window.open("").document.write(list);
+}
+
+function add_button(text, onclick, cssObj) {
+  cssObj = cssObj || {
+    position: "absolute",
+    bottom: "7%",
+    right: "4%",
+    "z-index": 3,
+  };
+  let button = document.createElement("button");
+  let btnStyle = button.style;
+  document.body.appendChild(button);
+  button.innerHTML = text;
+  button.onclick = onclick;
+  Object.keys(cssObj).forEach((key) => {
+    btnStyle[key] = cssObj[key];
+  });
+  return button;
+}
+function filter_link(link) {
+  if (!!link.match(filter_regex)) {
+    return true;
+  }
+  return false;
+}
+
+function get_links() {
+  let urls = document.querySelectorAll("a");
+  let links_and_names = {};
+  for (let i = 0; i < urls.length; i++) {
+    let nametext = urls[i].textContent;
+    // remove spaces and tabs.
+    let cleantext = nametext.replace(/\t|\s+/g, "").trim();
+    let cleanlink = urls[i].href;
+    if (!filter_results) {
+      links_and_names[cleanlink] = cleantext;
+    } else if (filter_link(cleanlink)) {
+      links_and_names[cleanlink] = cleantext;
+    }
+  }
+  // make_list(links_and_names);
+  make_table(links_and_names);
 }
 
 (function () {
   "use strict";
-  let urls = document.querySelectorAll("a");
-  let links_and_names = {};
-  const name_regex = new RegExp(/png|jpg/g);
-  let res = true;
-  for (let i = 0; i < urls.length; i++) {
-    let nametext = urls[i].textContent;
-    // remove whilte spaces and tabs
-    let cleantext = nametext.replace(/\t|\s+/g, "").trim();
-    let cleanlink = urls[i].href;
-    // uncomment this line to get all links
-    //let res = !!cleanlink.match(name_regex);
-    // remove white spaces and tabs
-    if (res) {
-      links_and_names[cleanlink] = cleantext;
-    }
-  }
-  //make_list(Object.keys(links_and_names));
-  make_table(links_and_names);
+
+  window.addEventListener("load", () => {
+    add_button("Get Links", get_links, {
+      position: "absolute",
+      bottom: "7%",
+      left: "4%",
+      "z-index": 3,
+    });
+    add_button("Get Links", get_links, {
+      position: "absolute",
+      bottom: "7%",
+      right: "4%",
+      "z-index": 3,
+    });
+  });
 })();
